0ef9a2d1f55fb02e7708d7580763b0ce2ed756a5
mengyyy <mengyyy@users.noreply.github.com>
2016-05-14 15:53:55 +0800

    Update TextHandler.java

--- webapp/src/main/java/cn/saymagic/weixin/server/handler/TextHandler.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/handler/TextHandler.java
@@ -13,7 +13,9 @@ public class TextHandler extends BaseHandler {
 	@Override
 	public String doHandleMsg(MsgRequest msgRequest) {
 		try {
-			return getResponseStringByContent(TulingUtil.getContentStr(null==msgRequest.getContent()?msgRequest.getRecognition():msgRequest.getContent(), msgRequest.getFromUserName()),msgRequest);
+		//	return getResponseStringByContent(TulingUtil.getContentStr(null==msgRequest.getContent()?msgRequest.getRecognition():msgRequest.getContent(), msgRequest.getFromUserName()),msgRequest);
+			return getResponseStringByContent("Get the text~~~~",msgRequest);
+			
 		} catch (IOException e) {
 			return getResponseStringByContent("TextHandler.java IOException错 ",msgRequest);
 		}
